289110f47eb3c789d10c9e37903f1f28d5efdb58
Ayush <tseegii.tseky@gmail.com>
2022-02-26 19:50:08 +0800

    event listeneruudiig vvsgej ogloo

---
+++ app.js
@@ -0,0 +1,24 @@
+var uiController = (function () {
+    
+});
+
+var financeController = (function () {
+    
+});
+
+var appController = (function (uiController, financeController) {
+   
+   var ctrlAddItem = function () {
+       console.log('enter');
+   };
+
+    document.querySelector('.add__btn').addEventListener('click', function () {
+      ctrlAddItem();
+    });
+document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+       ctrlAddItem
+    } 
+});
+
+})(uiController,financeController);
