7cce9cfe366fa9ab40e51550d708b629f3de772f
Ayush <tseegii.tseky@gmail.com>
2022-02-27 16:48:02 +0800

    orlogo zarlagiin delgetsen deer gargana

--- app.js
+++ app.js
@@ -18,8 +18,22 @@ return {
        };
    },
    getDOMstring : function () {
-       return DOMstring;
-       
+       return DOMstring;       
+   },
+   addListener : function (item, type) {
+       var html;
+       if (type === 'inc') {
+           list = ".income__list";
+           html = '<div class="item clearfix" id="income-$$id$$"><div class="item__description">$$description$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+       } else {
+        list = ".expenses__list";
+        html = '<div class="item clearfix" id="expenses-$$id$$"><div class="item__description">$$description$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+       };
+       html =html.replace('$$id$$', item.id);
+       html =html.replace('$$description$$', item.description);
+       html =html.replace('$$VALUE$$', item.value);
+       document.querySelector(list).insertAdjacentHTML('beforeend', html);
+
    }
 }; })();
 
@@ -41,7 +55,7 @@ var Expense = function(id, description, value){
     var data = {
         items :{
             inc : [],
-            exo : []
+            exp : []
         },
         totels : {
             inc : 0 ,
@@ -49,11 +63,11 @@ var Expense = function(id, description, value){
         }
     };
     return {
-        addItem: function(type,des, val) {
+        addItem: function(type, des, val) {
 var item, id;
 if (data.items[type].length === 0) id = 1;
  else {
- id =   data.items[type][data.items[type].length - 1].id +1;
+ id =   data.items[type][data.items[type].length - 1].id + 1;
 }
 
 if (type === 'inc') {
@@ -63,6 +77,8 @@ if (type === 'inc') {
 }
 
        data.items[type].push(item);
+
+       return item ;
         },
         seeData : function () {
            return data;
@@ -78,7 +94,9 @@ var ctrlAddItem = function () {
     var input = uiController.getInput();
     console.log(financeController.seeData());
 // financeController ruu oruulah
-    financeController.addItem(input.type, input.description, input.value);
+  var item =  financeController.addItem(input.type, input.description, input.value);
+  //olj awsan ogogdloo webiin tohiroh hesegt gargah
+  uiController.addListener(item, input.type);
 };
 
 var setupEventlisteners = function () {
