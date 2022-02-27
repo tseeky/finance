578fdd6fdde0a4ed3f01d284664f3483801fc776
Ayush <tseegii.tseky@gmail.com>
2022-02-27 15:02:59 +0800

    Finance module ruu item add hiideg bolgono

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+
+//Delgetsiin controller
 var uiController = (function(){
-var DOMstring = {
+//index,.html holbol
+    var DOMstring = {
 inputType : '.add__type',
 inputDesription : '.add__description',
 inputValue : '.add__value',
@@ -18,12 +21,12 @@ return {
        return DOMstring;
        
    }
-};
+}; })();
 
-})();
 // ********Sanhvvgin controller***********
 var financeController = (function(){
-var Income = function(id, description, value){
+
+    var Income = function(id, description, value){
 this.id = id ;
 this.description = description;
 this.value = value;
@@ -36,7 +39,7 @@ var Expense = function(id, description, value){
     };
 
     var data = {
-        allItems :{
+        items :{
             inc : [],
             exo : []
         },
@@ -44,14 +47,38 @@ var Expense = function(id, description, value){
             inc : 0 ,
             exp: 0
         }
-    }
+    };
+    return {
+        addItem: function(type,des, val) {
+var item, id;
+if (data.items[type].length === 0) id = 1;
+ else {
+ id =   data.items[type][data.items[type].length - 1].id +1;
+}
+
+if (type === 'inc') {
+    item = new Income(id, des, val);
+} else {
+    item = new Expense(id, des, val);
+}
+
+       data.items[type].push(item);
+        },
+        seeData : function () {
+           return data;
+        }
+    };
 
 })();
 //****************************** */
 
 var appController = (function(uiController,financeController){
 var ctrlAddItem = function () {
-    console.log(uiController.getInput());
+    //uiController ooroos irj bgaa utga awah
+    var input = uiController.getInput();
+    console.log(financeController.seeData());
+// financeController ruu oruulah
+    financeController.addItem(input.type, input.description, input.value);
 };
 
 var setupEventlisteners = function () {
